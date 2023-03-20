3dbb268b9cd846b7b5deb6b54c9e2199708ea414
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-03-20 21:42:21 +0100

    adding apis

--- src/apis/data-verification/sierra-leone/index.ts
+++ src/apis/data-verification/sierra-leone/index.ts
@@ -7,16 +7,15 @@ import { BaseSDK } from '../../base-config';
 
 export class SierraLeoneDatasVerification extends BaseSDK {
   //These APi are not tested yet
-  verifySlDriversLicense(
-//     datas: Omit<
-//       KenyaNationalIdentityParams,
-//       'number' | 'customer_name' | 'customer_reference'
-//     >
-//   ) {
-//     return this.post(VERIFY_NATIONAL_IDENTITY_SOUTH_AFRICA_ENDPOINT, datas);
-//   }
-
-//   verifySaBusiness(datas: VerifyBusSAParams) {
-//     return this.post(VERIFY_BUSINESS_SOUTH_AFRICA_ENDPOINT, datas);
-//   }
+  //   verifySlDriversLicense(
+  //     datas: Omit<
+  //       KenyaNationalIdentityParams,
+  //       'number' | 'customer_name' | 'customer_reference'
+  //     >
+  //   ) {
+  //     return this.post(VERIFY_NATIONAL_IDENTITY_SOUTH_AFRICA_ENDPOINT, datas);
+  //   }
+  //   verifySaBusiness(datas: VerifyBusSAParams) {
+  //     return this.post(VERIFY_BUSINESS_SOUTH_AFRICA_ENDPOINT, datas);
+  //   }
 }
